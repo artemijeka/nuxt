997c7ee4037e9f59d8771ce35851b31bf2d7b3d6
Artem <artem.kuznecov.samara@yandex.ru>
2022-08-09 11:19:15 +0400

    init

---
+++ NOTES.md
@@ -0,0 +1,28 @@
+﻿Docs
+    ...https://v3.nuxtjs.org
+        ...https://v3.nuxtjs.org/guide/features/data-fetching
+
+    https://nuxtjs.org
+
+
+
+Examples    
+    https://nuxtjs.org/examples/routing/dynamic-pages
+
+Tutorials
+    https://nuxtjs.org/tutorials/
+
+
+
+V Nuxt.js Fundamentals
+    V https://vueschool.io/lessons/nuxtjs-seo-and-meta-tags
+        делал в static-learn-refs
+
+...Jamstack: The Complete Guide
+    ...https://vueschool.io/courses/jamstack-the-complete-guide
+
+!Async Data with Nuxt.js
+    !https://vueschool.io/lessons/the-asyncdata-method-and-the-context
+        !далее платно
+            делаю в static-learn-refs 
+                только не posts а articles
---
+++ static-learn-refs/pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <Tutorial />
+    <nav>
+      <h2>Articles</h2>
+      <nuxt-link
+        v-for="article in articles"
+        :key="article.id"
+        :to="{ name: 'articles-id', params: { id: article.id } }"
+        class="main-page-nav__item"
+      >
+        {{ article.title }}
+      </nuxt-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      articles: [],
+    }
+  },
+  head() {
+    return {
+      title: 'Nuxt + Vue + Vuex | Home page!',
+      meta: [
+        { name: 'twitter:title', content: 'Nuxt + Vue + Vuex | Home page!' },
+        {
+          name: 'twitter:description',
+          content: 'Home page! Nuxt + Vue + Vuex = Power&Speed',
+        },
+        {
+          name: 'twitter:image',
+          content: 'https://i.ytimg.com/vi/yHrSHiBSP4U/maxresdefault.jpg',
+        },
+        { name: 'twitter:card', content: 'summary_large_image' },
+      ],
+    }
+  },
+  computed: {
+    // posts() {
+    //   return this.$store.state.posts.all
+    // },
+  },
+  mounted() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+      this.articles = response.data
+    })
+    // fetch('https://jsonplaceholder.typicode.com/posts').then((response) => {
+    //   response.json().then((articles) => {
+    //     this.articles = articles
+    //   })
+    // })
+  },
+}
+</script>
+
+<style lang="scss">
+.main-page-nav {
+  &__item {
+    margin-right: 10px;
+  }
+}
+</style>
